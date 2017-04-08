0f16ea062b6ee5359d1237753881a42643a710e2
jradbone <jradbone@radbonelaw.com>
2017-04-08 17:32:34 +0930

    Initial commit

---
+++ app/src/main/java/com/radbonelaw/firstproject1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.radbonelaw.firstproject1;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    int i = 2;
+
+    setContentView(R.layout.activity_main);
+  }
+//tHE RAIN IN sPAIN Falls mainly
+}
